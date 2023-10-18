72addd9c01fad78ff0f86c9a317a3c7cb85ff12e
HeavyTony2 <97621927+HeavyTony2@users.noreply.github.com>
2023-10-18 17:52:59 -0400

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,7 +5,15 @@ mkdir build
 cd build
 
 :: Configure using the CMakeFiles
-cmake -G "Ninja" -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE:STRING=Release ..
+cmake %CMAKE_ARGS% -GNinja ^
+      -DCMAKE_BUILD_TYPE=Release ^
+      -DCMAKE_PREFIX_PATH=%CONDA_PREFIX% ^
+      -DCMAKE_INSTALL_LIBDIR=%LIBRARY_LIB% ^
+      -DCMAKE_INSTALL_INCLUDEDIR=%LIBRARY_INC% ^
+      -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+      -DCMAKE_POSITION_INDEPENDENT_CODE=on ^
+      -DBUILD_SHARED_LIBS=ON
+
 if errorlevel 1 exit 1
 
 :: Build!
