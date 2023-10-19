d3e7cd0fc442f561d4c3827521804066f9fa9846
HeavyTony2 <97621927+HeavyTony2@users.noreply.github.com>
2023-10-19 13:21:36 -0400

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -7,9 +7,20 @@ cd build
 :: Configure using the CMakeFiles
 cmake -G "NMake Makefiles" ^
     -DCMAKE_BUILD_TYPE=Release ^
+    -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
+    -DCMAKE_INSTALL_LIBDIR="Library/lib" ^
+    -DCMAKE_INSTALL_BINDIR="Library/bin" ^
+    -DCMAKE_INSTALL_INCLUDEDIR="Library/include" ^
+    -DCMAKE_INSTALL_DATAROOTDIR="Library/share" ^
+    -DCMAKE_INSTALL_PREFIX="%PREFIX%" ^
+    -DCMAKE_PROGRAM_PATH=$BUILD_PREFIX/bin ^
       ..
 if errorlevel 1 exit 1
 
 :: Build!
 nmake
 if errorlevel 1 exit 1
+
+:: Install!
+nmake install
+if errorlevel 1 exit
