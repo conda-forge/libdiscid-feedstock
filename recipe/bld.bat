daaa18d99bb5a704e11f69eb0c00552fe0b57f2f
HeavyTony2 <97621927+HeavyTony2@users.noreply.github.com>
2023-10-19 09:40:25 -0400

    Update bld.bat
    Adding one line inspired by https://github.com/conda-forge/pugixml-feedstock/blob/main/recipe/bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -8,6 +8,7 @@ cd build
 cmake -G "NMake Makefiles" ^
       -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
       -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
+      -DBUILD_SHARED_LIBS:BOOL=ON ^
       -DCMAKE_BUILD_TYPE:STRING=Release ^
       ..
 if errorlevel 1 exit 1
