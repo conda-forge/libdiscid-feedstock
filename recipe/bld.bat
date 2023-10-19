60145d67050e5862f6aa51e08ee05ca3abaad217
HeavyTony2 <97621927+HeavyTony2@users.noreply.github.com>
2023-10-19 10:10:18 -0400

    Update bld.bat
    Insipired by https://github.com/conda-forge/ale-feedstock/blob/66c23baf1cc419c0c01bfc12eb507a8c2073ebb4/recipe/bld.bat#L11

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -6,13 +6,15 @@ cd build
 
 :: Configure using the CMakeFiles
 cmake -G "NMake Makefiles" ^
-      -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
-      -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
-      -DCMAKE_INSTALL_BINDIR="%LIBRARY_BIN%" ^
-      -DCMAKE_INSTALL_LIBDIR="%LIBRARY_LIB%" ^
-      -DBUILD_SHARED_LIBS:BOOL=ON ^
-      -DCMAKE_BUILD_TYPE:STRING=Release ^
-      ..
+        -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=ON ^
+        -DALE_BUILD_TESTS=OFF ^
+        -DCMAKE_BUILD_TYPE=Release ^
+        -DCMAKE_INSTALL_PREFIX="%PREFIX%" ^
+        -DCMAKE_INSTALL_LIBDIR="%LIBRARY_LIB%" ^
+        -DCMAKE_INSTALL_INCLUDEDIR="%LIBRARY_INC%" ^
+        -DCMAKE_INSTALL_BINDIR="%LIBRARY_BIN%" ^
+        -DCMAKE_INSTALL_DATADIR="%LIBRARY_PREFIX%" ^
+        ..
 if errorlevel 1 exit 1
 
 :: Build!
