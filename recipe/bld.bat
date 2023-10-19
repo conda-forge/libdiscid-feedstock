701828cae05e6b1d4dca59c1dfedd167a837ab86
HeavyTony2 <97621927+HeavyTony2@users.noreply.github.com>
2023-10-19 09:08:47 -0400

    Update bld.bat
    Template on https://conda-forge.org/docs/maintainer/knowledge_base.html

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,12 +5,17 @@ mkdir build
 cd build
 
 :: Configure using the CMakeFiles
-cmake -G "Ninja" -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%"   -DCMAKE_RUNTIME_OUTPUT_DIRECTORY:PATH="%LIBRARY_BIN%"  -DCMAKE_ARCHIVE_OUTPUT_DIRECTORY:PATH="%LIBRARY_LIB%" -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE:STRING=Release ..
+cmake -G "NMake Makefiles" ^
+      -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
+      -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
+      -DCMAKE_BUILD_TYPE:STRING=Release ^
+      ..
 if errorlevel 1 exit 1
 
 :: Build!
-ninja
+nmake
 if errorlevel 1 exit 1
 
-ninja install
-if errorlevel 1 exit 1
+:: Install!
+nmake install
+if errorlevel 1 exit
