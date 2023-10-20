d4bcf60923cf7805be33449a05b499f0a26b6359
HeavyTony2 <97621927+HeavyTony2@users.noreply.github.com>
2023-10-20 10:20:49 -0400

    Restoring ninja in the bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,25 +5,18 @@ mkdir build
 cd build
 
 :: Configure using the CMakeFiles
-cmake -G "NMake Makefiles" ^
-    -DCMAKE_BUILD_TYPE=Release ^
-    -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
-    -DCMAKE_INSTALL_LIBDIR="Library/lib" ^
-    -DCMAKE_INSTALL_BINDIR="Library/bin" ^
-    -DCMAKE_INSTALL_INCLUDEDIR="Library/include" ^
-    -DCMAKE_INSTALL_DATAROOTDIR="Library/share" ^
-    -DCMAKE_INSTALL_PREFIX="%PREFIX%" ^
-    -DCMAKE_PROGRAM_PATH=$BUILD_PREFIX/bin ^
-      ..
+cmake -G "Ninja" -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" -DCMAKE_BUILD_TYPE:STRING=Release ..
+if errorlevel 1 exit 1
+
 if errorlevel 1 exit 1
 
 :: Build!
-nmake VERBOSE=1
+ninja VERBOSE=1
 if errorlevel 1 exit 1
 
 nmake discid discisrc
 if errorlevel 1 exit 1
 :: Install!
-nmake install VERBOSE=1
+ninja install VERBOSE=1
 mv  %LIBRARY_LIB%\discid.dll %LIBRARY_BIN%\discid.dll -v 
 if errorlevel 1 exit
